bfa294343d27c34385185df65841904252382387
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-03-17 14:45:12 -0400

    Added missing icons

--- ramses-portfolio/src/Components/skills.js
+++ ramses-portfolio/src/Components/skills.js
@@ -1,8 +1,8 @@
 import React from "react";
-import NodeIcon from "../images/iconfinder_nodejs-new-pantone-black_1012818.png";
-import ReactIcon from "../iconfinder_React.js_logo_1174949.png";
+import NodeIcon from "../images/NodeIcon.png";
+import ReactIcon from "../images/ReactIcon.png";
 import JsIcon from "../images/icons8-javascript-96.png";
-import ReduIcon from "../images/reduxLogo.png"
+import ReduxIcon from "../images/reduxLogo.png";
 import "../App/App.css";
 
 function Skills() {
@@ -13,6 +13,42 @@ function Skills() {
         A collection of technologies I've grown fluent in and utilize in my day
         to day to create my best work.
       </h3>
+      <div className="Skill-Icon-Box">
+        <ul className="Skill-Icon-Grid">
+          <li className="Icon-Style Icon-1">
+            <img alt="icon" src={NodeIcon} />
+            <h3>Node.js</h3>
+          </li>
+          <li className="Icon-Style Icon-2">
+            <img className="Icon-Img" alt="icon" src={ReactIcon} />
+            <h3>React</h3>
+          </li>
+          <li className="Icon-Style Icon-3">
+            <img className="Icon-Img" alt="icon" src={JsIcon} />
+            <h3>Javascript</h3>
+          </li>
+          <li className="Icon-Style Icon-4">
+            <img className="Icon-Img" alt="icon" src={ReduxIcon} />
+            <h3>Redux</h3>
+          </li>
+          <li className="Icon-Style Icon-5">
+            <img />
+            <h3>HTML5</h3>
+          </li>
+          <li className="Icon-Style Icon-6">
+            <img />
+            <h3>CSS</h3>
+          </li>
+          <li className="Icon-Style Icon-7">
+            <img />
+            <h3>LESS</h3>
+          </li>
+          <li className="Icon-Style Icon-8">
+            <img />
+            <h3>MongoDB</h3>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
