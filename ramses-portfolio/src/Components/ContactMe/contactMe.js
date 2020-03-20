d880880a226ca13647f7a9dd9593d90ac9886cb0
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-03-19 22:41:13 -0400

    mobile styling complete

--- ramses-portfolio/src/Components/ContactMe/contactMe.js
+++ ramses-portfolio/src/Components/ContactMe/contactMe.js
@@ -31,34 +31,3 @@ function ContactMe() {
 }
 
 export default ContactMe;
-
-// function ContactMe() {
-//   return (
-//     <div id="Contact Me" className="Contact-Box Section">
-//       <h2 className="Center-underline">Contact Me</h2>
-//       <h3>
-//         Ready to join your team and collaborate in the future of software
-//         engineering
-//       </h3>
-//       <div className="Info-Box">
-//         <span className="Info-Row">
-//           <dt>Location</dt>
-//           <dd>Miami, FL &bull; USA</dd>
-//         </span>
-//         <span className="Info-Row">
-//           <dt>Phone</dt>
-//           <dd>(786) 309-0172</dd>
-//         </span>
-//         <span className="Info-Row">
-//           <dt>Email</dt>
-//           <dd>
-//             gbmudez@gmail.com
-//           </dd>
-//         </span>
-//       </div>
-//       <a href={Resume} className="SeeCode-Btn">
-//         View Resume
-//       </a>
-//     </div>
-//   );
-// }
